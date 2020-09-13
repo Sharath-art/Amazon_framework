84245a7853537520fc898a61776be98602d6ebf8
shara <shara@LAPTOP-PK0IHITU>
2020-09-13 19:19:22 +0530

    itime login

--- Amazon/src/test/java/com/amazon/testcases/TestcasewithoutFW.java
+++ Amazon/src/test/java/com/amazon/testcases/TestcasewithoutFW.java
@@ -4,26 +4,50 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
+import org.testng.log4testng.Logger;
 
-public class TestcasewithoutFW {
+import com.amazon.pages.BaseClass;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestcasewithoutFW extends BaseClass {
 
 	@Test
 	 public void test1(){
-		
+		//set up browser
 		System.setProperty("webdriver.chrome.driver", "./Drivers/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://amazon.in");
+		 driver = new ChromeDriver();
+		/*
+		 * WebDriverManager.chromedriver().setup(); WebDriver driver = new
+		 * ChromeDriver();
+		 */
+		driver.get("https://itimer.jkt.in/itimer/login.htm");
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
-		driver.findElement(By.xpath("//span[@class='nav-line-1' and text()='Hello. Sign in']")).click();
-		driver.findElement(By.id("ap_email")).sendKeys("sharath.rnsit.118@gmail.com");
-		driver.findElement(By.id("continue")).click();
-		driver.findElement(By.id("ap_password")).sendKeys("data$123");
-		driver.findElement(By.id("signInSubmit")).click();
 		
+		//login 
+		logger = report.createTest("Login to itimer");
+		driver.findElement(By.id("employeeCode")).sendKeys("Gouthami.sr@jktech.com");
+		driver.findElement(By.id("employeepassword")).sendKeys(")Q+QZ^c!");
+		driver.findElement(By.id("loginbutton")).click();
+		
+		//
+		logger.info("home page displayed");
+		WebElement report = driver.findElement(By.linkText("Reports"));
+		WebDriverWait wait = new WebDriverWait(driver, 30);
+		wait.until(ExpectedConditions.elementToBeClickable(report));
+		report.click();
+		driver.findElement(By.xpath("//a[text()='Reports']/following-sibling::ul//a[contains(.,'Timesheet')]")).click();
+		WebElement txtResourceName = driver.findElement(By.id("resourceName"));
+		logger.info("report page displayed");
+		wait.until(ExpectedConditions.visibilityOf(txtResourceName));
 		
 		
 	}
